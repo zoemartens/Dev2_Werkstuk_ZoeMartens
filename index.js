db0974a14f5273a3bb6a4245e40bfc9eac6696f1
Zoë Martens <zoe.martens@student.ehb.be>
2020-04-07 11:19:04 +0200

    Get weather from API

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+let form = document.getElementById('form');
+form.addEventListener('submit', getWeather);
 
+async function getWeather(event){
+    event.preventDefault();
+    console.log('Form submit event');
+
+    //Get input value
+    let search = document.getElementById('search');
+    console.log(search.value);    
+
+    //Do API call
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search.value}&APPID=d7b955c4c268fe54649d6f0d702b39d1&units=metric`);
+    console.log(response);
+    const data = await response.json();
+    console.log('Data', data);
+    console.log('Place', data.name);
+    console.log(data.main.temp);
+    console.log(data.weather[0].description);
+
+    let htmlString1 = `<h2>Weather:</h2>
+    <ul>
+      <li>Place: ${data.name}</li>
+      <li>Temperature: ${data.main.temp}</li>
+      <li>Description: ${data.weather[0].description}</li>
+    </ul>
+    `;
+
+    let htmlString2 = `<h2>Favorites:</h2>`;
+
+
+  //Inject into HTML
+  console.log('htmlString1', htmlString1);
+        let listHtml = document.getElementById('weatherResult');
+        listHtml.innerHTML= htmlString1;
+  console.log('htmlString2', htmlString2);
+        let listHtml2 = document.getElementById('favorites')
+        listHtml2.innerHTML = htmlString2;
+}
