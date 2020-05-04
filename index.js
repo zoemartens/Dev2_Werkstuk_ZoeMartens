3a5537507c594cbc7fa5a398967191c67b0b214f
Zoë Martens <zoe.martens@student.ehb.be>
2020-05-04 12:51:45 +0200

    website af,nu css

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 "use strict";
+//FIREBASE CODE
+firebase.initializeApp({
+  apiKey: 'AIzaSyDIjwkCBF5lBORnYBJmzXp1BmV2gCSOy-A',
+  projectId: 'werkstuk-zoemartens-4ba50'
+});
 
+const database = firebase.firestore();
+const placeCollection = database.collection("place");
+
+const convertQuerySnapshotToRegularArray = (querySnapshot) => querySnapshot.docs.map((item) => ({ id: item.id, ...item.data() }));
+
+async function renderPlace(){
+  placeCollection.onSnapshot((querySnapshot) => {
+
+    const place = convertQuerySnapshotToRegularArray(querySnapshot);
+    console.log(place);
+
+    //START CODE
 let form = document.getElementById('form');
 form.addEventListener('submit', getWeather);
 
 async function getWeather(event){
     event.preventDefault();
-    console.log('Form submit event');
+    console.log('Form submit');
 
     //Get input value
     let search = document.getElementById('search');
@@ -19,6 +36,7 @@ async function getWeather(event){
     console.log('Place', data.name);
     console.log(data.main.temp);
     console.log(data.weather[0].description);
+    
 
     let htmlString1 = 
     `<ul>
@@ -27,6 +45,18 @@ async function getWeather(event){
       <li>Description: ${data.weather[0].description}</li>
     </ul>
     `;
+
+//GEPROBEERD MAAR WERKT NIET
+    /*let celsius = `${data.main.temp}`;
+
+    function convertToF(celsius){
+      if(document.getElementsByClassName("slider")=true){
+        return celsius * 9/5 + 32
+      }else{
+        return celcius;
+      }
+    }
+    convertToF();*/
 
 
   //Inject into HTML
@@ -37,13 +67,17 @@ async function getWeather(event){
 let formFavorites = document.getElementById('savedList');
 savedList.addEventListener('submit', makeFavorite);
 
-async function makeFavorite(event){
+function makeFavorite(event){
   event.preventDefault();
   console.log('make favorite begins');
   let htmlString2 = document.getElementById('favoritesResult').innerHTML += htmlString1;
 
 
-  }
-
-
+      }
+      
+    }
+    
+  });
 }
+
+renderPlace();
